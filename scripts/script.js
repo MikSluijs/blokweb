fee857b8bae3d4b071372cbd5800a24c47a38959
MikSluijs <90104895+MikSluijs@users.noreply.github.com>
2021-09-25 11:53:37 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 var menu = document.querySelector("header nav ");
-var uitklapMenu = document.querySelector("header nav ul");
+var uitklapMenu = document.querySelector("header nav ");
 
 menu.addEventListener("click", openMenu);
 
